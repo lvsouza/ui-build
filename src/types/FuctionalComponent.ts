7b5fb60976b710aab9b3bcb82ee67b43219e67a0
lvsouza <ls18017@gmail.com>
2021-03-30 12:58:32 -0300

    refactor: improved Functional component type

--- src/types/FuctionalComponent.ts
+++ src/types/FuctionalComponent.ts
@@ -1,4 +1,11 @@
 
-export type FuctionalComponent<T = {}, O = HTMLElement> = (props: T) => O;
-export type FC<I = {}, O = HTMLElement> = FuctionalComponent<I, O>;
+type FuctionalComponentWithProps<T = any, O = HTMLElement> = (props: T) => O;
+type FuctionalComponentWithoutProps<O = HTMLElement> = () => O;
+
+export type FuctionalComponent<T = null, O = HTMLElement> =
+  T extends null
+  ? FuctionalComponentWithoutProps<O>
+  : FuctionalComponentWithProps<T, O>;
+
+export type FC<I = null, O = HTMLElement> = FuctionalComponent<I, O>;
 
